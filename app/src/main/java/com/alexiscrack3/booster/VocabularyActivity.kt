1a7945ddcbf6c149d9291dbcfbd4485f0b58b76a
Alexis Ortega <alexis.oc3@gmail.com>
2019-09-02 11:40:00 -0600

    Rename main activity to vocabulary activity

--- app/src/main/java/com/alexiscrack3/booster/VocabularyActivity.kt
+++ app/src/main/java/com/alexiscrack3/booster/VocabularyActivity.kt
@@ -3,10 +3,10 @@ package com.alexiscrack3.booster
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class VocabularyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_vocabulary)
     }
 }
