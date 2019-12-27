b079ed61ecebcad068810a9c5381dc48f0bd09a5
Alexis Ortega <alexis.oc3@gmail.com>
2019-12-27 15:45:54 -0800

    Test click event on view holder

--- app/src/test/java/com/alexiscrack3/booster/vocabulary/entries/EntryViewHolderTest.kt
+++ app/src/test/java/com/alexiscrack3/booster/vocabulary/entries/EntryViewHolderTest.kt
@@ -3,6 +3,9 @@ package com.alexiscrack3.booster.vocabulary.entries
 import com.alexiscrack3.booster.BoosterTest
 import com.alexiscrack3.booster.R
 import com.alexiscrack3.booster.models.Entry
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import kotlinx.android.synthetic.main.item_entry.view.*
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
@@ -20,10 +23,30 @@ class EntryViewHolderTest : BoosterTest() {
             "prounciation",
             emptyList()
         )
-        
         val testObject = EntryViewHolder(view)
+
         testObject.bind(entry, {})
 
         assertThat(view.entry_headword_text_view.text.toString(), equalTo(entry.headword))
     }
+
+    @Test
+    fun `when bound click event is set`() {
+        val entry = Entry(
+            "id",
+            "headword",
+            "class",
+            "prounciation",
+            emptyList()
+        )
+        val testObject = EntryViewHolder(view)
+        val onClick = mock<OnClickHandler>()
+        testObject.bind(entry, onClick)
+
+        testObject.itemView.performClick()
+
+        verify(onClick).invoke(entry)
+    }
+
+    private interface OnClickHandler: (Entry) -> Unit
 }
