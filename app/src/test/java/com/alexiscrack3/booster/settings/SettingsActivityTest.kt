4185bce37ce98cbde70b6fd493ba089ea3dabdd5
Alexis Ortega <alexis.oc3@gmail.com>
2020-02-28 10:30:43 -0700

    Use PAUSED looper mode to make test pass

--- app/src/test/java/com/alexiscrack3/booster/settings/SettingsActivityTest.kt
+++ app/src/test/java/com/alexiscrack3/booster/settings/SettingsActivityTest.kt
@@ -5,15 +5,17 @@ import com.alexiscrack3.booster.BoosterTest
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
+import org.robolectric.annotation.LooperMode
 
+@LooperMode(LooperMode.Mode.PAUSED)
 class SettingsActivityTest : BoosterTest() {
 
-//    @Test
-//    fun `shows settings screen`() {
-//        val activityScenario = ActivityScenario.launch(SettingsActivity::class.java)
-//        activityScenario.onActivity { activity ->
-//            val fragment = activity.supportFragmentManager.fragments.first()
-//            assertThat(fragment, instanceOf(SettingsFragment::class.java))
-//        }.close()
-//    }
+    @Test
+    fun `shows settings screen`() {
+        val activityScenario = ActivityScenario.launch(SettingsActivity::class.java)
+        activityScenario.onActivity { activity ->
+            val fragment = activity.supportFragmentManager.fragments.first()
+            assertThat(fragment, instanceOf(SettingsFragment::class.java))
+        }.close()
+    }
 }
