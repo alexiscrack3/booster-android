5db0dd6f54caf3d34f35aa9e88d72a8f341e3e03
Alexis Ortega <alexis.ortega@wwt.com>
2020-03-09 11:05:30 -0600

    Test home fragment

--- app/src/test/java/com/alexiscrack3/booster/home/HomeFragmentTest.kt
+++ app/src/test/java/com/alexiscrack3/booster/home/HomeFragmentTest.kt
@@ -1,5 +1,7 @@
 package com.alexiscrack3.booster.home
 
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.fragment.app.testing.launchFragmentInContainer
 import com.alexiscrack3.booster.BoosterTest
@@ -7,6 +9,7 @@ import com.alexiscrack3.booster.R
 import com.alexiscrack3.booster.settings.SettingsActivity
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
@@ -15,7 +18,20 @@ import org.robolectric.Shadows.shadowOf
 class HomeFragmentTest : BoosterTest() {
 
     @Test
-    fun `settings item returns true`() {
+    fun `onCreateOptionsMenu inflates options menu`() {
+        val fragmentScenario = launchFragmentInContainer<HomeFragment>()
+        fragmentScenario.onFragment { fragment ->
+            val menu = mock<Menu>()
+            val inflater = mock<MenuInflater>()
+
+            fragment.onCreateOptionsMenu(menu, inflater)
+
+            verify(inflater).inflate(R.menu.options, menu)
+        }
+    }
+
+    @Test
+    fun `onOptionsItemSelected returns true when settings item is selected`() {
         val fragmentScenario = launchFragmentInContainer<HomeFragment>()
         fragmentScenario.onFragment { fragment ->
             val item = mock<MenuItem> {
@@ -28,7 +44,7 @@ class HomeFragmentTest : BoosterTest() {
     }
 
     @Test
-    fun `default item returns false`() {
+    fun `onOptionsItemSelected returns false when item does not match any id`() {
         val fragmentScenario = launchFragmentInContainer<HomeFragment>()
         fragmentScenario.onFragment { fragment ->
             val item = mock<MenuItem>()
