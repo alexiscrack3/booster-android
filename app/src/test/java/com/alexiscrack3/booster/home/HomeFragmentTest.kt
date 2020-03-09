7f7a8b9e525dfe029c7a0f76ca16f4a6e7337b27
Alexis Ortega <alexis.ortega@wwt.com>
2020-03-09 11:01:18 -0600

    Test home fragment

--- app/src/test/java/com/alexiscrack3/booster/home/HomeFragmentTest.kt
+++ app/src/test/java/com/alexiscrack3/booster/home/HomeFragmentTest.kt
@@ -1,29 +1,54 @@
 package com.alexiscrack3.booster.home
 
+import android.view.MenuItem
 import androidx.fragment.app.testing.launchFragmentInContainer
 import com.alexiscrack3.booster.BoosterTest
-import kotlinx.android.synthetic.main.fragment_home.*
+import com.alexiscrack3.booster.R
+import com.alexiscrack3.booster.settings.SettingsActivity
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
+import org.robolectric.Shadows.shadowOf
 
 class HomeFragmentTest : BoosterTest() {
 
     @Test
-    fun `entries button emits vocabulary navigation event`() {
+    fun `settings item returns true`() {
         val fragmentScenario = launchFragmentInContainer<HomeFragment>()
         fragmentScenario.onFragment { fragment ->
-            fragment.home_entries_button.performClick()
+            val item = mock<MenuItem> {
+                on { this.itemId } doReturn R.id.settings
+            }
+            val actual = fragment.onOptionsItemSelected(item)
 
-//            verify(router).navigate(BoosterNavigationEvent.Vocabulary)
+            assertThat(actual, equalTo(true))
         }
     }
 
     @Test
-    fun `settings button emits settings navigation event`() {
+    fun `default item returns false`() {
         val fragmentScenario = launchFragmentInContainer<HomeFragment>()
         fragmentScenario.onFragment { fragment ->
-            fragment.home_settings_button.performClick()
+            val item = mock<MenuItem>()
+            val actual = fragment.onOptionsItemSelected(item)
 
-//            verify(router).navigate(BoosterNavigationEvent.Play)
+            assertThat(actual, equalTo(false))
+        }
+    }
+
+    @Test
+    fun `settings screen is started when settings item is selected`() {
+        val fragmentScenario = launchFragmentInContainer<HomeFragment>()
+        fragmentScenario.onFragment { fragment ->
+            val item = mock<MenuItem> {
+                on { this.itemId } doReturn R.id.settings
+            }
+            fragment.onOptionsItemSelected(item)
+
+            val actual = shadowOf(context).nextStartedActivity.component?.className
+            assertThat(actual, equalTo(SettingsActivity::class.java.name))
         }
     }
 }
