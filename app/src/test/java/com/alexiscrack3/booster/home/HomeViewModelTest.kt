eff63afd3e75a0242483d9093a669e4373718afd
Alexis Ortega <alexis.ortega@wwt.com>
2020-03-09 10:49:05 -0600

    Test home view model

--- app/src/test/java/com/alexiscrack3/booster/home/HomeViewModelTest.kt
+++ app/src/test/java/com/alexiscrack3/booster/home/HomeViewModelTest.kt
@@ -1,28 +1,38 @@
 package com.alexiscrack3.booster.home
 
+import android.view.View
+import androidx.navigation.NavController
+import com.alexiscrack3.booster.R
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import org.junit.Test
 
 class HomeViewModelTest {
 
-//    @Test
-//    fun `onEntriesClick emits vocabulary event`() {
-//        val router = mock<BoosterRouter>()
-//        val testObject = HomeViewModel(router)
-//
-//        testObject.onEntriesClick()
-//
-//        verify(router).navigate(BoosterNavigationEvent.Vocabulary)
-//    }
-//
-//    @Test
-//    fun `onPlayClick emits play event`() {
-//        val router = mock<BoosterRouter>()
-//        val testObject = HomeViewModel(router)
-//
-//        testObject.onPlayClick()
-//
-//        verify(router).navigate(BoosterNavigationEvent.Play)
-//    }
+    @Test
+    fun `onEntriesClick emits vocabulary event`() {
+        val navController = mock<NavController>()
+        val view = mock<View>()
+        whenever(view.getTag(androidx.navigation.R.id.nav_controller_view_tag)).thenReturn(navController)
+//        whenever(view.findNavController()).thenReturn(navController)
+
+        val testObject = HomeViewModel()
+        testObject.onEntriesClick(view)
+
+        verify(navController).navigate(R.id.action_homeFragment_to_vocabulary_nav_graph)
+    }
+
+    @Test
+    fun `onPlayClick emits play event`() {
+        val navController = mock<NavController>()
+        val view = mock<View>()
+        whenever(view.getTag(androidx.navigation.R.id.nav_controller_view_tag)).thenReturn(navController)
+//        whenever(view.findNavController()).thenReturn(navController)
+
+        val testObject = HomeViewModel()
+        testObject.onPlayClick(view)
+
+        verify(navController).navigate(R.id.action_homeFragment_to_playFragment)
+    }
 }
